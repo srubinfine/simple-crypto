9fee3d13db9d163032710679198f28e16b828e1b
Slava Rubinfine <slava@weareadaptive.com>
2022-01-31 21:30:53 -0500

    fix sim impl - first draft

---
+++ fix-simulator/src/main/java/com/adgarsolutions/SimulatorPublisher.java
@@ -0,0 +1,15 @@
+package com.adgarsolutions;
+
+import com.adgarsolutions.shared.model.Order;
+import io.micronaut.configuration.kafka.annotation.KafkaClient;
+import io.micronaut.configuration.kafka.annotation.KafkaKey;
+import io.micronaut.configuration.kafka.annotation.Topic;
+
+import javax.inject.Singleton;
+
+@Singleton
+@KafkaClient
+public interface SimulatorPublisher {
+    @Topic("order")
+    void sendOrder(@KafkaKey String orderId, Order order);
+}
---
+++ fix-simulator/src/main/java/com/adgarsolutions/SimulatorStarter.java
@@ -0,0 +1,47 @@
+package com.adgarsolutions;
+
+import io.micronaut.context.annotation.Value;
+import io.micronaut.context.event.StartupEvent;
+import io.micronaut.runtime.event.annotation.EventListener;
+import io.micronaut.scheduling.annotation.Scheduled;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.adgarsolutions.shared.model.Order;
+
+import javax.inject.Singleton;
+import java.util.UUID;
+
+@Singleton
+public class SimulatorStarter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SimulatorStarter.class);
+
+    private final SimulatorPublisher simulatorPublisher;
+
+    public SimulatorStarter(SimulatorPublisher simulatorPublisher) {
+        this.simulatorPublisher = simulatorPublisher;
+    }
+
+    @Value("${order.post.simulator-mode}")
+    private Boolean simulatorMode;
+
+    @EventListener
+    void onStartup(StartupEvent startupEvent) {
+        LOG.info("Simulator mode {}", enabledSimulationMode() ? "ENABLED" : "DISABLED");
+    }
+
+    @Scheduled(fixedDelay = "${order.post.execute}")
+    void postOrder() {
+
+        if (enabledSimulationMode()) {
+            var o =  new Order(UUID.randomUUID().toString());
+            LOG.info("Submitting order to Kafka: {}", o.getId());
+            // Uncomment when make sure Kafka cluster is running: simulatorPublisher.sendOrder(o.getId(), o);
+            // TODO: Will need to add service to check if cluster is up on startup
+        }
+    }
+
+    private boolean enabledSimulationMode() {
+        return this.simulatorMode != null && this.simulatorMode.booleanValue() == true;
+    }
+}
