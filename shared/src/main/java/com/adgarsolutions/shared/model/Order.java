1964f44ad2003fab873da55851e4ff47c16f7eea
Slava Rubinfine <slava@weareadaptive.com>
2022-02-01 20:10:52 -0500

    added properties to Order

--- shared/src/main/java/com/adgarsolutions/shared/model/Order.java
+++ shared/src/main/java/com/adgarsolutions/shared/model/Order.java
@@ -4,9 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Order {
     private String id;
+    private String symbol;
+    private BigDecimal price;
+    private BigDecimal shares;
+    private String side;
 }
