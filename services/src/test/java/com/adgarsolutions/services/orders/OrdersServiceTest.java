ea5e08ed4d31ae92f129f7b5fb4425c3125895cb
Slava Rubinfine <slava@weareadaptive.com>
2022-02-08 10:17:07 -0500

    added flux from file and resource file

--- services/src/test/java/com/adgarsolutions/services/orders/OrdersServiceTest.java
+++ services/src/test/java/com/adgarsolutions/services/orders/OrdersServiceTest.java
@@ -2,32 +2,37 @@ package com.adgarsolutions.services.orders;
 
 import com.adgarsolutions.shared.model.Order;
 import org.junit.jupiter.api.Test;
-import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
-import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 import reactor.util.function.Tuple2;
-import reactor.util.function.Tuple3;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.function.Function;
 
 class FilePublisher {
-    public static Flux<String> createFlux(String fileName) {
+
+    public static Flux<String> createFluxFromFile(Function<String, BufferedReader> f, String fileNameOrPath) throws IOException {
+        var reader = new BufferedReader(new FileReader(new File(fileNameOrPath)));
+        return Flux.create(s -> {
+            String line;
+            try {
+                while (null != (line = reader.readLine())) {
+                    s.next(line);
+                }
+                s.complete();
+            } catch (Exception ex) {
+                s.error(ex);
+            }
+        }, FluxSink.OverflowStrategy.BUFFER);
+    }
+
+    public static Flux<String> createFluxFromResourceFile(String fileName) throws IOException {
         var reader = new BufferedReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream(fileName)));
         return Flux.create(s -> {
             String line;
+
             try {
                 while (null != (line = reader.readLine())) {
                     s.next(line);
@@ -43,16 +48,16 @@ class FilePublisher {
 class OrdersServiceTest {
 
     @Test
-    void getAllOrdersForAccount() {
+    void getAllOrdersForAccount() throws IOException {
 
         // NOTE:  there are 3 symbols in the file, but only 2 idx, so zip is going to generate ONLY 2 recs
-        Flux.just(1,2).zipWith(FilePublisher.createFlux("test1.txt"))
+        Flux.just(1,2).zipWith(FilePublisher.createFluxFromResourceFile("test1.txt"))
                 .map(OrdersServiceTest::mapToOrder1).subscribe(System.out::println);
 
         // OR using static
 
         Flux f1 = Flux.just(1,2,3,4,5);
-        Flux f2 = FilePublisher.createFlux("test1.txt");
+        Flux f2 = FilePublisher.createFluxFromResourceFile("test1.txt");
 
         // EITHER 2 LINES OR CAST Flux<Tuple2<String, Integer>> zipped = (Flux<Tuple2<String, Integer>>)Flux.zip(f2, f1);
         // zipped.map(OrdersServiceTest::mapToOrder2).subscribe(System.out::println);
