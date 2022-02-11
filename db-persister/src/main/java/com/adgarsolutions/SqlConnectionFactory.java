eb5faf5940f0586b055c3bdb5e72dd7221410533
Slava Rubinfine <slava@weareadaptive.com>
2022-02-11 10:46:13 -0500

    ability to create connection per request

--- db-persister/src/main/java/com/adgarsolutions/SqlConnectionFactory.java
+++ db-persister/src/main/java/com/adgarsolutions/SqlConnectionFactory.java
@@ -3,6 +3,8 @@ package com.adgarsolutions;
 import com.adgarsolutions.config.DbSourceConfiguration;
 import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Primary;
+import jdk.jfr.Name;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,16 +17,39 @@ public class SqlConnectionFactory {
 
     private final static Logger LOG = LoggerFactory.getLogger(SqlConnectionFactory.class);
 
+    private Connection connection;
+
     @Bean
-    public Connection getConnection(DbSourceConfiguration dbSourceConfiguration) throws SQLException {
+    @Primary
+    public Connection getSharedConnection(DbSourceConfiguration dbSourceConfiguration) throws SQLException {
+       return getConnection(dbSourceConfiguration, false);
+    }
+
+    private synchronized Connection getConnection(
+            DbSourceConfiguration dbSourceConfiguration,
+            boolean createNewOnEachRequest) throws SQLException
+    {
+        if (createNewOnEachRequest || this.connection == null) {
+            createConnection(dbSourceConfiguration);
+        }
+
+        return this.connection;
+    }
+
+    @Bean
+    public Connection getConnectionPerRequest(DbSourceConfiguration dbSourceConfiguration) throws SQLException {
+        return getConnection(dbSourceConfiguration, true);
+    }
+
+    private Connection createConnection(DbSourceConfiguration dbSourceConfiguration) throws SQLException {
         String connectionUrl = String.format("%s?currentSchema=%s&user=%s&password=%s", // TODO: SSL
                 dbSourceConfiguration.getUrl(), dbSourceConfiguration.getDefaultSchema(), dbSourceConfiguration.getUser(),
                 dbSourceConfiguration.getPassword());
 
         try {
-            var connection = DriverManager.getConnection(connectionUrl);
+            this.connection = DriverManager.getConnection(connectionUrl);
             LOG.info("Connection created SUCCESS");
-            return connection;
+            return this.connection;
         } catch (Exception ex) {
             LOG.error("FAILED CREATING CONNECTION: {}", ex.getMessage());
             throw ex;
